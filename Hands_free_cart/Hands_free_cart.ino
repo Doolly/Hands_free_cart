e39d2dea35604745ed95d7b204d3e86ad16b850d
Doolly <treeland88@hotmail.com>
2016-05-23 01:30:33 +0900

    게인 수정 직전
    ㅇㅇ

--- Hands_free_cart/Hands_free_cart.ino
+++ Hands_free_cart/Hands_free_cart.ino
@@ -29,9 +29,6 @@ void Find();
 void GetDistance (void);
 void Change_Value_in_Serial();
 /*------ Global Variables ------*/
-const int distance_throtle = 1000;
-int distance;
-int distance_error;
 int PWM_L;
 int PWM_R;
 int mode;
@@ -45,52 +42,34 @@ void setup() {
   pinMode(R_MOTER_F, OUTPUT);
   pinMode(L_MOTER_B, OUTPUT);
   pinMode(R_MOTER_F, OUTPUT);
-  pinMode(DIS_SENSOR, INPUT);
   pixy.init();
   InitMoter();
   MsTimer2::set(100, LostTimer);  //100ms
   MsTimer2::start();
   Serial.begin(9600);
   Serial.println("Lets go");
-  BTSerial.begin(9600);
+
 }
 
 void loop() {
-    while (mode == 0) { //자동모드
-      object_cnt = pixy.getBlocks();
-  
-      if (object_cnt != 0 ) {
-        target = Look(object_cnt);
-        Follow(target);
-        finder_var = 0;
-      }
-      if (distance < 20) { //장애물 감지 정지
-        LeftMoterCtrl(0);
-        RightMoterCtrl(0);
-      }
-      else if (object_cnt = 0 && finder_var > 10) { //일정시간 이상 타겟이 없다면
-        Find();
-      }
-    }
-  
 
-  while (mode == 0) { //자동모드
+//  while (mode == 0) { //자동모드
 
     object_cnt = pixy.getBlocks();
-
+    Serial.print("object_cnt = " + String(object_cnt) + "\n");
     if (object_cnt != 0 ) {
       target = Look(object_cnt);
       Follow(target);
       finder_var = 0;
     }
-    if (distance < 30) { //장애물 감지 정지
-      LeftMoterCtrl(0);
-      RightMoterCtrl(0);
-    }
-    if ((object_cnt == 0) && (finder_var > 20)) { //2초 시간 이상 타겟이 없다면
-      Find();
-    }
-  }
+//    if (distance < 30) { //장애물 감지 정지
+//      LeftMoterCtrl(0);
+//      RightMoterCtrl(0);
+//    }
+//    if ((object_cnt == 0) && (finder_var > 20)) { //2초 시간 이상 타겟이 없다면
+//      Find();
+//    }
+//  }
   //  while (mode == 1) { //수동모드
   //
   //  }
